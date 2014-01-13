a769f60a765c25edb32a34f170603d2e7558649a
Christopher Millward <christopher.millward@gmail.com>
2014-01-13 08:49:48 -0500

    Added Meter texture and started implementation.

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -5,6 +5,7 @@ using System.Collections;
 /// Controls a single wave game object.
 /// </summary>
 public class Wave : MonoBehaviour {
+	GameState gameState;
 	Vector3 start_position;
 	tk2dSprite sprite;
 
@@ -23,6 +24,7 @@ public class Wave : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		gameState = GameObject.FindGameObjectWithTag("World").GetComponent<GameState>();
 		start_position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 		sprite = GetComponent<tk2dSprite>();
 				
@@ -32,9 +34,11 @@ public class Wave : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!IsActive) {
-			Debug.Log(currentLifetime);
+		if (gameState.State != GameStateEnum.Running) {
+			return;
+		}
 
+		if (!IsActive) {
 			GameObject.Destroy(gameObject);
 			return;
 		}
