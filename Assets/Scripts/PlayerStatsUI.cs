ff54727df6a4d5e665bd58d1cd5ea5841b0098f4
Christopher Millward <christopher.millward@gmail.com>
2014-01-13 22:46:42 -0500

    Added visual meters to replace the text-based meters.

--- Assets/Scripts/PlayerStatsUI.cs
+++ Assets/Scripts/PlayerStatsUI.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class PlayerStatsUI : MonoBehaviour {
 	private PlayerController player;
-	private tk2dTextMesh meters;
+
+	public tk2dClippedSprite relaxationProgress;
+	public tk2dClippedSprite bladderProgress;
+	public tk2dClippedSprite hungerProgress;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-		meters = GetComponent<tk2dTextMesh>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		meters.text = string.Format("Relaxation: {0:00}%\nBladder: {1:00}%\nHunger: {2:00}%", player.Relaxation, player.Bladder, player.Hunger);
-		meters.Commit();
+		relaxationProgress.clipTopRight = new Vector2(player.Relaxation / 100.0f, relaxationProgress.clipTopRight.y);
+		bladderProgress.clipTopRight = new Vector2(player.Bladder / 100.0f, bladderProgress.clipTopRight.y);
+		hungerProgress.clipTopRight = new Vector2(player.Hunger / 100.0f, hungerProgress.clipTopRight.y);
 	}
 }
