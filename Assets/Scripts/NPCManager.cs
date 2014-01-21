8269168fed2192defcb6d069ead08d9d757a1800
Christopher Millward <christopher.millward@gmail.com>
2014-01-21 08:03:57 -0500

    Replaced pre-set NPCs with NPCs at random locations.

---
+++ Assets/Scripts/NPCManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCManager : MonoBehaviour {
+	public GameObject[] NPCTypes;	// Types of NPCs to generate.
+	public int MaxNPCs = 0;			// Max items that can be onscreen at once. Zero for infinite.
+
+	private MovementGrid movementGrid;
+
+	// Use this for initialization
+	void Start () {
+		movementGrid = GameObject.FindGameObjectWithTag("Movement Grid").GetComponent<MovementGrid>();
+
+		for (int i = 0; i < MaxNPCs; ++i) {
+			GenerateNPC();
+		}
+	}
+
+	void GenerateNPC() {
+		// Pick a new row / column for the item to be created at.
+		List<GridSquare> potentialSquares = new List<GridSquare>();
+		for (int row = 0; row < movementGrid.NumRows; row++) {
+			for (int column = 0; column < movementGrid.NumColumns; column++) {
+				GridSquare gridSquare = movementGrid.SquarePositions[row][column];
+				if (gridSquare.IsTraversable && gridSquare.IsOccupied() == false && gridSquare.Component == null) {
+					potentialSquares.Add (gridSquare);	
+				}
+			}
+		}
+		if (potentialSquares.Count == 0) {
+			Debug.Log("Couldn't find a valid square for the new NPC. Bailing.");
+			return;
+		}
+		GridSquare newGridSquare = potentialSquares[Random.Range(0, potentialSquares.Count)];
+		
+		// Create a new NPC.
+		int npcIndex = Random.Range(0, NPCTypes.Length);
+		GameObject newNPC = (GameObject)GameObject.Instantiate(NPCTypes[npcIndex]);
+		
+		// Figure out what kind of NPC this is.
+		if (newNPC.GetComponent<NPCAggressive>()) {
+			newNPC.GetComponent<NPCAggressive>().StartRow = newGridSquare.Row;
+			newNPC.GetComponent<NPCAggressive>().StartColumn = newGridSquare.Column;
+		}
+		else if (newNPC.GetComponent<NPCBasic>()) {
+			newNPC.GetComponent<NPCBasic>().StartRow = newGridSquare.Row;
+			newNPC.GetComponent<NPCBasic>().StartColumn = newGridSquare.Column;	
+		}
+	}
+}
