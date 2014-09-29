df58d3f342eb01ea5091554f8e9b7adb218c7283
Christopher Millward <christopher.millward@gmail.com>
2014-09-29 07:42:23 -0400

    Added horizontal movement and size changesto sunrise.

--- Assets/Scripts/Sunrise.cs
+++ Assets/Scripts/Sunrise.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class Sunrise : MonoBehaviour {
 	GameTimer timer;
 	public float TargetHeight = 28.0f;
-	float changePerSecond;
+	float heightChangePerSecond;
+
+	public float TargetWidth = 0.5f;
+	float widthChangePerSecond;
+
+	public float TargetXPos = 50.0f;
+	float xPosChangePerSecond;
+
 	bool isRunning = false;
+	private float startXPos = 0.0f;
 	private float startHeight = 0.0f;
+	private float startWidth = 0.0f;
 	
 	/// <summary>
 	/// Awake hook.
@@ -15,22 +24,28 @@ public class Sunrise : MonoBehaviour {
 		MessageManager.Instance.RegisterListener(new Listener("GameStateChange", gameObject, "OnGameStateChange"));
 
 		startHeight = transform.position.y;
+		startWidth = transform.localScale.x;
+		startXPos = transform.position.x;
 	}
 	
 	// Use this for initialization
 	void Start () {
 		timer = GameObject.FindGameObjectWithTag("World").GetComponent<GameTimer>();
-		changePerSecond = TargetHeight / (timer.duration * 0.5f);
-
+		heightChangePerSecond = (TargetHeight - startHeight) / (timer.duration * 0.5f);
+		widthChangePerSecond = (TargetWidth - startWidth) / (timer.duration * 0.5f);
+		xPosChangePerSecond = (TargetXPos - startXPos) / timer.duration;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isRunning) {
 			float direction = timer.Elapsed() / timer.duration < 0.5f ? 1.0f : -1.0f;
-			float change = Time.deltaTime * direction * changePerSecond;
+			float heightChange = Time.deltaTime * direction * heightChangePerSecond;
+			float widthChange = Time.deltaTime * direction * widthChangePerSecond;
+			float xPosChange = Time.deltaTime * xPosChangePerSecond;
 			
-			transform.Translate(new Vector3(0, change, 0));
+			transform.Translate(new Vector3(xPosChange, heightChange, 0));
+			transform.localScale += new Vector3(widthChange, widthChange, 0);
 		}
 	}
 	
@@ -54,8 +69,9 @@ public class Sunrise : MonoBehaviour {
 			isRunning = false;
 
 			// Reset the position of the sun.
-			Vector3 newPosition = new Vector3(transform.position.x, startHeight, transform.position.z);
+			Vector3 newPosition = new Vector3(startXPos, startHeight, transform.position.z);
 			transform.position = newPosition;
+			transform.localScale = new Vector3(startWidth, transform.localScale.y, transform.localScale.z);
 			break;
 		default:
 			break;
